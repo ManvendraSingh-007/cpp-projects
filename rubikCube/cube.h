b06215596419aebf3678c3420c1b4c0c3f35612a
ManvendraSingh-007 <montchouhan@gmail.com>
2025-10-03 08:43:40 +0530

    added some static constexpr variables (general 3x3 cube Properties) and declare function inputCubeState

--- rubikCube/cube.h
+++ rubikCube/cube.h
@@ -3,25 +3,31 @@
 
 #include <array>
 
-class Cube {
+class Cube
+{
 
 private:
     using Cube3x3 = std::array<std::array<std::array<int, 3>, 3>, 6>;
     Cube3x3 cube_arr_;
 
-public:
+    void validate_input();
 
+public:
     Cube();
     Cube(const Cube3x3 &initial_data_);
     Cube(const int (&c_style_data_)[6][3][3]);
 
-    static Cube3x3 get_array_type() {
+    static Cube3x3 get_array_type()
+    {
         return {};
     }
 
     void displayCube();
+    void inputCubeState();
 
-   
+    static constexpr int face{6};
+    static constexpr int row{3};
+    static constexpr int column{3};
 };
 
 #endif
